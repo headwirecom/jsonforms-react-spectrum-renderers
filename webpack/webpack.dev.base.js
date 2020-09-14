c178eb7a560108535558e669aa6b37f3f6487caf
Max Burri <burri@puzzle.ch>
2020-09-14 16:06:38 +0200

    formatted file

--- webpack/webpack.dev.base.js
+++ webpack/webpack.dev.base.js
@@ -3,28 +3,30 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
 module.exports = merge(baseConfig, {
-    mode: 'development',
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/dev-server',
-        './src/index.ts',
-        './example/index.ts'
-    ],
-    output: {
-        publicPath: "/assets/",
-        filename: "bundle.js"
-    },
+  mode: 'development',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './src/index.ts',
+    './example/index.ts'
+  ],
+  output: {
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
 
-    devServer: {
-        contentBase: './example'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        rules: [
-            { test: /\.html$/, exclude: /node_modules/, loader: 'html-loader?exportAsEs6Default' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-        ]
-    },
+  devServer: {
+    contentBase: './example'
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'html-loader?exportAsEs6Default'
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  }
 });
