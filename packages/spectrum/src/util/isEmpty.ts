547e4e1b0cdcf230a3c9dd2d9800f49791ef03cd
Ruben <ruben@LAPTOP-2BUVE01N.localdomain>
2022-10-03 06:31:29 -0700

    clean branch

--- packages/spectrum/src/util/isEmpty.ts
+++
@@ -1,4 +0,0 @@
-// https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_isempty
-export function isEmpty(obj: any) {
-  return [Object, Array].includes((obj || {}).constructor) && !Object.keys((obj || {})).length;
-}
